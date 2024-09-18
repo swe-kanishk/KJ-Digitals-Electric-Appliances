2942a2de35d2bc43c9516de912e041571f87b631
kanishk tiwari <kanishk.swe@gmail.com>
2024-09-18 06:13:33 +0530

    first commit

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -1,7 +1,7 @@
-import sectionImage1 from '../public/Best ceiling fan.jpeg'
-import sectionImage2 from '../public/Longway _ Creative Ads _ Oven Toaster _ Coffee and Creative.jpeg'
-import sectionImage3 from '../public/Longway _ Kitchen Appliances _ Mixer Grinder _ Coffee and Creative.jpeg'
-import sectionImage4 from '../public/PHILIPS _ Smart TV.jpeg'
+import sectionImage1 from '../../public/Best ceiling fan.jpeg'
+import sectionImage2 from '../../public/Longway _ Creative Ads _ Oven Toaster _ Coffee and Creative.jpeg'
+import sectionImage3 from '../../public/Longway _ Kitchen Appliances _ Mixer Grinder _ Coffee and Creative.jpeg'
+import sectionImage4 from '../../public/PHILIPS _ Smart TV.jpeg'
 
 const images = [
     {
