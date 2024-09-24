c9936ba30ec2761d94575c3c4397baf43af3a557
kanishk tiwari <kanishk.swe@gmail.com>
2024-09-24 15:58:05 +0530

    make landing ui responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,45 +48,90 @@ function Home() {
         </div>
       </marquee>
 
-      <section className="relative flex justify-between h-[85vh]">
-        <div className="flex flex-wrap justify-center pt-[6%] items-start pl-6 flex-col gap-4 w-1/2">
-          <h1 className="text-[2rem] font-bold">Smart Living Starts Here<br /> Elevate Your Home with Cutting-Edge Electronics</h1>
-          <p className="text-gray-400 font-medium">Transform your lifestyle with KJ Digitals! Explore the latest in electronic appliances that combine sleek design with unbeatable performance. Upgrade today and live smarter.</p>
-          <div className="my-6">
-            <button className="bg-red-500 text-white py-2 px-3 font-medium rounded-lg flex items-center gap-2">See all available products <FaArrowCircleRight size={'20px'} /></button>
+      <section className="relative flex flex-col lg:flex-row justify-between min-h-[85vh]">
+        <div className="flex xl:flex-wrap xl:justify-center pt-[6%] pb-[6%] lg:pb-0 flex-1 items-start lg:pl-6 flex-col w-full gap-4 xl:w-1/2">
+          <div className="text-center lg:text-start mx-auto px-3 flex flex-col gap-4">
+            <h1 className="md:text-[2rem] text-[1.2rem] sm:text-[1.5rem] font-bold">
+              Smart Living Starts Here
+              <br /> Elevate Your Home with Cutting-Edge Electronics
+            </h1>
+            <p className="text-gray-400 mx-auto lg:mx-0 xl:w-auto w-full text-[14px] sm:text-[18px] md:font-medium">
+              Transform your lifestyle with KJ Digitals! Explore the latest in
+              electronic appliances that combine sleek design with unbeatable
+              performance. Upgrade today and live smarter.
+            </p>
+            <Link
+              to="/products"
+              className="bg-red-500 mx-auto lg:mx-0 xl:self-start w-fit hover:bg-red-600 text-white py-2 px-3 font-medium rounded-lg flex items-center gap-2"
+            >
+              See all available products <FaArrowCircleRight size={"20px"} />
+            </Link>
           </div>
-          <div className="flex gap-3 flex-1 items-end justify-between">
-              <img src="https://i.pinimg.com/564x/f0/a3/ef/f0a3efaa6be4fac83e00d06181735e8c.jpg" className="rounded-2xl my-3 w-2/4" alt="" />
+          <div className="flex gap-3 w-full xl:flex-1 lg:items-end h-full pr-6 justify-between">
+            <img
+              src="https://i.pinimg.com/564x/f0/a3/ef/f0a3efaa6be4fac83e00d06181735e8c.jpg"
+              className="rounded-2xl hidden lg:flex self-end my-3 w-2/4"
+              alt=""
+            />
 
-            <div className="h-fit self-end my-3 rounded-2xl flex-1 overflow-hidden w-full relative bg-purple-700">
-              <img src="https://i.pinimg.com/564x/bb/46/cc/bb46cce207cc28ca37d6c70854560c25.jpg" alt="" />
+            <div className="h-fit flex lg:hidden xl:flex self-end my-3 rounded-2xl flex-1 overflow-hidden w-full relative ">
+              <img
+                className="hidden lg:flex lg:rounded-2xl"
+                src="https://i.pinimg.com/564x/bb/46/cc/bb46cce207cc28ca37d6c70854560c25.jpg"
+                alt=""
+              />
               <ul className="absolute flex flex-col gap-1 top-4 left-4 font-semibold">
-              <span className="text-white pb-3">Why KJ Digitals ?</span>
+                <span className="text-white pb-3">Why KJ Digitals ?</span>
                 <li className="flex items-center gap-2 text-sm text-black">
-                <TiArrowRight />Top-Quality Appliances from Trusted Brands.
+                  <TiArrowRight />
+                  Top-Quality Appliances from Trusted Brands.
                 </li>
                 <li className="flex items-center gap-2 text-sm text-black">
-                <TiArrowRight />Affordable Prices with Amazing Discounts.
-
+                  <TiArrowRight />
+                  Affordable Prices with Amazing Discounts.
                 </li>
                 <li className="flex items-center gap-2 text-sm text-black">
-                <TiArrowRight />Fast, Reliable Shipping Nationwide.
-
+                  <TiArrowRight />
+                  Fast, Reliable Shipping Nationwide.
                 </li>
                 <li className="flex items-center gap-2 text-sm text-black">
-                <TiArrowRight />Expert Support Available 24/7.
+                  <TiArrowRight />
+                  Expert Support Available 24/7.
                 </li>
               </ul>
-              <Link to="/contact" className="absolute bottom-5 right-5 text-white rounded-lg bg-black px-4 py-2 hover:bg-white hover:text-black font-medium">Contact us!</Link>
+              <Link
+                to="/contact"
+                className="absolute bottom-5 right-5 text-white z-20 border-white border-2 rounded-lg bg-black px-4 py-2 hover:bg-purple-700 font-medium"
+              >
+                Contact us!
+              </Link>
             </div>
+            <div className="relative w-fit lg:hidden flex justify-end">
+            <img
+              src="https://i.pinimg.com/564x/69/4c/1f/694c1f3e615fb3419496ddf7ec675098.jpg"
+              className="absolute max-h-[300px] h-[250px] z-10 rounded-xl bottom-0 right-[30%]  border-4 border-white"
+              alt=""
+            />
+            <img
+              className="rounded-xl lg:w-auto relative left-[1rem] w-1/2"
+              src="https://i.pinimg.com/564x/e7/91/58/e79158085645e47113f4488b8807c3e3.jpg"
+              alt=""
+            />
+          </div>
           </div>
         </div>
-        <div>
-          <div className="relative">
-            <img src="https://i.pinimg.com/564x/69/4c/1f/694c1f3e615fb3419496ddf7ec675098.jpg" className="absolute h-[400px] rounded-xl bottom-0 right-[70%] border-4 border-white" alt="" />
-            <img className="rounded-xl" src="https://i.pinimg.com/564x/e7/91/58/e79158085645e47113f4488b8807c3e3.jpg" alt="" />
+          <div className="relative hidden lg:flex justify-end">
+            <img
+              src="https://i.pinimg.com/564x/69/4c/1f/694c1f3e615fb3419496ddf7ec675098.jpg"
+              className="absolute max-h-[300px] lg:h-[400px] z-10 rounded-xl bottom-0 right-[40%] lg:right-[70%] border-4 border-white"
+              alt=""
+            />
+            <img
+              className="rounded-xl lg:w-auto relative left-[1rem] w-1/2"
+              src="https://i.pinimg.com/564x/e7/91/58/e79158085645e47113f4488b8807c3e3.jpg"
+              alt=""
+            />
           </div>
-        </div>
       </section>
 
       <section className="bg-[#151515] px-3 py-4 flex flex-col gap-3">
